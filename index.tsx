e65589b220d6a4dbd3e22657f6e83dc0ce423e37
Victor Saiz <155447748+vicalbahaca@users.noreply.github.com>
2026-01-26 09:46:21 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Analytics } from "@vercel/analytics/react"
+/* import { Analytics } from "@vercel/analytics/react" */
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
