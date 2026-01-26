0d35bef0f8d1e97abd6b059542292494bc04c2fe
Victor Saiz <155447748+vicalbahaca@users.noreply.github.com>
2026-01-26 09:33:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/',
+        base: '/AAAQuest/',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify('AIzaSyB8-sRNMg4Qft-LItYeYQmUbzbK7qfUrcE'),
-        'process.env.GEMINI_API_KEY': JSON.stringify('AIzaSyB8-sRNMg4Qft-LItYeYQmUbzbK7qfUrcE')
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
